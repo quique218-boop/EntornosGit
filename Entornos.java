f90717cba6b8f5af9fc7d1166a95c50f362af90c
quique218-boop <quique218@alumnes.ilerna.com>
2025-11-07 11:44:02 +0100

    añadido nuevo mensaje

--- Entornos.java
+++ Entornos.java
@@ -1,1 +1,6 @@
-public class Entornos{public static void main(String[] args){System.out.println("HolaGit)}}
+public class Entornos{
+    public static void main(String[] args){
+        System.out.println("HolaGit");
+        System.out.println("Lolazo");
+        }
+        }
